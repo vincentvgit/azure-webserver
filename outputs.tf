0d3bc80dbf4b5d91cca5ed09fd1f93dbf5600739
Nouma2016 <abdel.nouma@gmail.com>
2023-05-28 17:06:39 +0000

    First commit

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "The_subnet_ID" {
+ value = azurerm_subnet.webserver-subnet.id
+}
+
+output "The_vnet_ID" {
+ value = azurerm_virtual_network.webserver-net.id
+}
+
+output "The_websrever_Private_ip" {
+   value = azurerm_linux_virtual_machine.nginx.private_ip_address
+}
+
+output "The_webserver_Public_ip" {
+   value = azurerm_linux_virtual_machine.nginx.public_ip_address
+}
