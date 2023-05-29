855ce9730eaf7ce39f1e0c9307e9c731c35da31a
vincentvgit <vincentsimplon@gmail.com>
2023-05-29 14:09:02 +0200

    tp13 en cours

--- init-script.sh
+++ init-script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 #Installing Docker
-...
-...
+
+curl -fsSL https://get.docker.com -o install-docker.sh
+
+sh install-docker.sh --dry-run
+
+sudo sh install-docker.sh
+
 
 #Creating container
-...
-...
+
+sudo docker pull vincentvdocker/mynginx2:latest
+
+sudo docker run -p 8080:80 vincentvdocker/mynginx2:latest
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "webserver" {
-   name = "nginx-server"
+   name = "vincent-nginx-server"
    location = var.location
 }
 
@@ -92,8 +92,8 @@ resource "azurerm_linux_virtual_machine" "nginx" {
    }
 
    computer_name = "nginx"
-   admin_username = "adminuser"
-   admin_password = "Faizan@bashir.123"
+   admin_username = "vincent"
+   admin_password = "Azertyuiop123"
    disable_password_authentication = false
 
    os_disk {
